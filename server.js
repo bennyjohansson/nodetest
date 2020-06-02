69491ab257586fd12f98cf4168d75c2f8e73e14f
Benny <johansson.benny@gmail.com>
2020-06-02 06:28:14 +0200

    testnode 1

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
+
+const port = process.port.env.port || 3000;
+app.listen(port, function () {
+
+
+    console.log('Forst API running on port 3000');
+})
