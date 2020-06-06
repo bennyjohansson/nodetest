745812f96e837e67ba1ed327f968f96fa467e6b7
Benny <johansson.benny@gmail.com>
2020-06-06 12:54:57 +0200

    specifiex port in log

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res, next) => {
+
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+  
+});
 
 const port = process.env.port || 3000;
 app.listen(port, function () {
 
 
     console.log('Forst API running on port 3000');
+    console.log(port);
 })
